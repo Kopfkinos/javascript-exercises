918f09e00d61e7b1c44ba449b63a7f4f8040d655
IgelIntelligenz <connor_v_d_murphy@hotmail.com>
2025-07-14 17:02:04 +0100

    completed task 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  const args = [a, b]
 
-};
+  const invalidArgs = args.some((arg) => {
+    return arg < 0 || typeof arg !== "number" || arg % 1 !== 0
+  })
+
+  if (invalidArgs) {
+    return "ERROR"
+  }
+
+  const bigger = Math.max(a, b)
+  const smaller = Math.min(a, b)
+
+  let sum = 0
+  for (let i = smaller; i <= bigger; i++) {
+    sum += i
+  }
+  return sum
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
