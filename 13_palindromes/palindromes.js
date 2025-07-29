12d52605906db057b8ffb9166c98fcb45cb8d575
IgelIntelligenz <connor_v_d_murphy@hotmail.com>
2025-07-29 23:36:45 +0100

    completed task 13 palindrome

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+  str = str.toLowerCase()
+  const onlyLetters = str.match(/[a-z0-9]+/g).join("")
+  const leng = onlyLetters.length - 1
+  const middle = Math.ceil(leng / 2)
+  let isPalindrome = true
+  for (let i = 0; i < middle / 2; i++) {
+    if (onlyLetters[i] !== onlyLetters[leng - i]) {
+      isPalindrome = false
+    }
+  }
+  return isPalindrome
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
