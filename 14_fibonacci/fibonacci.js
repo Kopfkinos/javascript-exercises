aec3438022cb5d231c7f86573c87b2a66730ea97
IgelIntelligenz <connor_v_d_murphy@hotmail.com>
2025-07-30 22:42:49 +0100

    completed fibonacci task

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = Number(num)
+  if (num < 0 || isNaN(num)) {
+    return "OOPS"
+  }
 
-};
+  if (num === 0) {
+    return 0
+  }
+  let currentFibNum = 1
+  let lastFibNum = 0
+  let secondLastFibNum = 0
+  for (let i = 1; i < num; i++) {
+    secondLastFibNum = lastFibNum
+    lastFibNum = currentFibNum
+    currentFibNum = secondLastFibNum + lastFibNum
+  }
+  return currentFibNum
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
